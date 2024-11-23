127324293d5cf183e3814778b0170d6edb0ea9d3
Seyoung0914 <seyoung8529@gmail.com>
2024-11-23 16:05:57 +0900

    implement ListPage

---
+++ src/components/pages/ListPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ShowList from './ShowList';
+
+const ListPage = () => (
+  <div className="container">
+    <h1>음악 목록</h1>
+    <ShowList />
+  </div>
+);
+
+export default ListPage;
